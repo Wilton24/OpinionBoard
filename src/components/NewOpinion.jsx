dbacd11b8e47baa9ffe9035a09a2e151a0e37b03
Wilton Batiquin <cbasher24@gmail.com>
2025-08-09 01:02:27 +0800

    Added form validation

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,8 +1,42 @@
 export function NewOpinion() {
+
+  function handleShareOpinion(formData) {
+    const name = formData.get('userName');
+    const title = formData.get('title');
+    const body = formData.get('body');
+
+    let errors = [];
+
+    if (title.trim().lenghth < 5) {
+      errors.push('Title must be at least 5 characters long.');
+    };
+
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push('Opinion must be between 10 and 300 characters long.');
+    };
+
+    if (name.trim().length < 2) {
+      errors.push('Name must be at least 2 characters long.');
+    };
+
+    if (errors.length > 0) {
+      return {
+        errors, enteredValues: {
+          name,
+          title,
+          body
+        }
+      };
+    }
+    return { errors: null }
+  };
+
+
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={handleShareOpinion}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
